598985d21f9d7a22b7c8dc84b765f55a33a82449
David Smit <dsmitty166@gmail.com>
2015-01-28 22:31:42 -0600

    Revert add OpenDelta

--- config/common.mk
+++ config/common.mk
@@ -14,8 +14,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
-    ro.build.selinux=1 \
-    ro.delta.version=VERSION
+    ro.build.selinux=1 
 
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
@@ -61,8 +60,7 @@ PRODUCT_PACKAGES += \
     Eleven \
     Launcher3 \
     LockClock \
-    OTACenter \
-    OpenDelta
+    OTACenter 
 
 # Busybox
 PRODUCT_PACKAGES += \
