9733e912d3ad4d5fd6ae850804e166dbe7119b26
Adnan Begovic <adnan@cyngn.com>
2015-08-11 15:51:54 -0700

    vendor/cm: Expose internal revision for platform.
    TICKET: CYNGNOS-724
    Change-Id: I9d93ca77fd888046cb4733a73ace2ee337c46300

--- config/common.mk
+++ config/common.mk
@@ -103,12 +103,6 @@ PRODUCT_PACKAGES += \
     Profiles \
     SlimLauncher
 
-# CM Platform Library
-PRODUCT_PACKAGES += \
-    org.cyanogenmod.platform-res \
-    org.cyanogenmod.platform \
-    org.cyanogenmod.platform.xml
-
 # Busybox
 PRODUCT_PACKAGES += \
     Busybox
@@ -126,6 +120,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=1
 
+# CM Platform Library
+PRODUCT_PACKAGES += \
+    org.cyanogenmod.platform-res \
+    org.cyanogenmod.platform \
+    org.cyanogenmod.platform.xml
+
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
     org.cyanogenmod.hardware \
@@ -138,12 +138,26 @@ ifndef CM_PLATFORM_SDK_VERSION
   # the SDK are released.  It should only be incremented when the APIs for
   # the new release are frozen (so that developers don't write apps against
   # intermediate builds).
-  CM_PLATFORM_SDK_VERSION := 1
+  CM_PLATFORM_SDK_VERSION := 2
 endif
+
+ifndef CM_PLATFORM_REV
+  # For internal SDK revisions that are hotfixed/patched
+  # Reset after each CM_PLATFORM_SDK_VERSION release
+  # If you are doing a release and this is NOT 0, you are almost certainly doing it wrong
+  CM_PLATFORM_REV := 0
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.cm.display.version=$(CM_DISPLAY_VERSION)
 
 # CyanogenMod Platform SDK Version
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.cm.build.version.plat.sdk=$(CM_PLATFORM_SDK_VERSION)
+
+# CyanogenMod Platform Internal
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.cm.build.version.plat.rev=$(CM_PLATFORM_REV)
 
 # T-Mobile theme engine
 include vendor/benzo/config/themes_common.mk
