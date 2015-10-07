6b37933bd8751b5498545dbf1ca37b7500d7fa5a
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-07 12:57:22 -0500

    sepolicy: remove BOARD_SEPOLICY_UNION
    * this is a no-op now
    
    Change-Id: I2023aaa633ea56310146516d261cc6bb8d99b3f3

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -2,8 +2,5 @@
 # This policy configuration will be used by all products that
 #
 
-BOARD_SEPOLICY_DIRS := \
+BOARD_SEPOLICY_DIRS += \
     vendor/benzo/sepolicy
-
-BOARD_SEPOLICY_UNION := \
-    mac_permissions.xml
