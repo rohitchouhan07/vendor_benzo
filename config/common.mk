a02156c5f8bf88654f4506369be779a81545ce6a
beanstown106 <nbenis106@gmail.com>
2015-12-18 00:34:44 -0800

    aosp_fixes: add missing google error reciever prop

--- config/common.mk
+++ config/common.mk
@@ -14,6 +14,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=dd-MM-yyyy \
     ro.com.android.dataroaming=false \
