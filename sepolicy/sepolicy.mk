Euphoria-OS/android_vendor_eos

sepolicy: Start Common sepolicy
Rather than having to maintain out of tree changes, it is often
easier to maintain a hiearchy of changes, starting with the vendors
common config file. From there, inheriting products can pick up a base
and start to add or remove certain bits from it, making use of the
BOARD_SEPOLICY_* functions documented in external/sepolicy/README.

Change-Id: I28a4aaf6c126535f0a88001582641b234a750015

sepolicy/sepolicy.mk
@@ -1,0 +1,9 @@
+#
+# This policy configuration will be used by all products that
+#
+
+BOARD_SEPOLICY_DIRS := \
+    vendor/eos/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+    mac_permissions.xml

rohitchouhan07/vendor_benzo

sepolicy/sepolicy.mk
@@ -1,0 +1,9 @@
+#
+# This policy configuration will be used by all products that
+#
+
+BOARD_SEPOLICY_DIRS := \
+    vendor/benzo/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+    mac_permissions.xml