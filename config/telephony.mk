5212d04279777153d3136ba8982196ae300dfb9e
mydongistiny <jaysonedson@gmail.com>
2016-02-05 18:23:49 -0800

    apns: Bring in new APN's and SPN's

--- config/telephony.mk
+++ config/telephony.mk
@@ -1,6 +1,12 @@
-# World APN list
-#PRODUCT_COPY_FILES += \
-#    vendor/benzo/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+# World APN lists
+PRODUCT_COPY_FILES += \
+    vendor/benzo/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    vendor/benzo/prebuilt/common/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml
+
+# World SPN lists
+PRODUCT_COPY_FILES += \
+    vendor/benzo/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml \
+    vendor/benzo/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
 
 # Telephony packages
 PRODUCT_PACKAGES += \
