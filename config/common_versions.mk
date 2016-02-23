924737652338bf45840b2ec88d864c0605c2f5c5
mydongistiny <jaysonedson@gmail.com>
2016-02-22 21:07:49 -0800

    DTC: Pull version and version to use from vendor (2/2)
    Also see build

--- config/common_versions.mk
+++ config/common_versions.mk
@@ -25,3 +25,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.ota.device=$(TARGET_DEVICE) \
     ro.ota.manifest=http://oceighty.co/ota/ota.xml
 
+# DragonTC info
+DRAGONTC_VERSION := 3.9
+export $(DRAGONTC_VERSION)
+
+DTC_PATH := prebuilts/clang/linux-x86/host/$(DRAGONTC_VERSION)
+DTC_VER := $(shell cat $(DTC_PATH)/VERSION)
+export $(DTC_VER)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.dtc.version=$(DTC_VER)
+
